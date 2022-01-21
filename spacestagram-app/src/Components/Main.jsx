22f9e49c5976c2b0ec9358febac89db246d68d42
jyu7771992 <jyu7771992@gmail.com>
2022-01-21 01:38:02 -0500

    create comment block with open and close function

--- spacestagram-app/src/Components/Main.jsx
+++ spacestagram-app/src/Components/Main.jsx
@@ -1,33 +1,161 @@
 
 import './Main.css';
 import React, {Component} from 'react';
+
 class Main extends Component{
   constructor(){
     super();
-
       this.state = {
-        posters: []
+        posters: [],
+        comments:[
+          {
+            id: "uidhrtsdori",
+            comment: "Omg! I can't believe this!",
+          },
+          {
+            id: "kkk123r45",
+            comment: 'MASTERPIECE!!',
+          },
+          {
+            id: "qqihjkfl",
+            comment: 'How did you create this?'
+          },
+          {
+            id: "l18385",
+            comment: "I swear I must to go to the space!!",
+          },
+          {
+            id: "kkk123r45",
+            comment: 'MASTERPIECE!!',
+          },
+          {
+            id: "9wefnkf",
+            comment: 'Unbelievable... '
+          }],
+          isCommentOpen_One: false,
+          isCommentOpen_Two: false,
+          isCommentOpen_Three: false,
       };
+    
   }
+  
   componentDidMount(){
     fetch('https://api.nasa.gov/planetary/apod?api_key=n55baMV2Dva4VjGvTZRENY6dscdSqaaf3cfEPoDs')
     .then(response => response.json())
     .then(post=> this.setState({ posters: post }));
   }
 
+  isCommentOpen_One(){
+    this.setState((preState) => ({
+      isCommentOpen_One: !preState.isCommentOpen_One 
+    }));
+  }
+  isCommentOpen_Two(){
+    this.setState((preState) => ({
+      isCommentOpen_Two: !preState.isCommentOpen_Two 
+    }));
+  }
+  isCommentOpen_Three(){
+    this.setState((preState) => ({
+      isCommentOpen_Three: !preState.isCommentOpen_Three 
+    }));
+  }
+
   render(){
     let post_info = [this.state.posters];
-    console.log(post_info);
+    const comment = this.state.comments;
     return (
       <div className="Main">
         {
           post_info.map((index, post) => (
             <div key={index} className="card">
               <h4 className="card-title">{post_info[post].title}</h4>
-              <span className="card-date">{post_info[post].date}</span>
-              <img src={post_info[post].hdurl} alt="" />
+              <div className="card-detail">
+                <span className="card-author">Author: {post_info[post].copyright}</span>
+                <span className="card-date"> Date: {post_info[post].date}</span>
+              </div>
+              <div className="card-images">
+                <img src={post_info[post].hdurl} alt="" />
+              </div> 
               <p className="card-explanation">{post_info[post].explanation}</p>
-              <button>Like</button>
+              <div className="card-btn-block">
+                <button className="card-btn like">Like</button>
+                <button className="card-btn comment" onClick={()=> this.isCommentOpen_One()}>Comments</button>
+                <button className="card-btn share">Share</button>
+              </div>
+              <ul className={`card-comment ${this.state.isCommentOpen_One ? "open" : "close"}`}>
+                {
+                  comment.map((user) => (
+                    <li className="user">
+                      <span className="user-id">{user.id}</span>
+                      <span className="user-comment">{user.comment}</span>
+                    </li>
+                  ))
+                } 
+                <button className="load-btn">Load More...</button>
+              </ul>
+            </div>
+          ))
+        }
+        {
+          post_info.map((index, post) => (
+            <div key={index} className="card">
+              <h4 className="card-title">{post_info[post].title}</h4>
+              <div className="card-detail">
+                <span className="card-author">Author: {post_info[post].copyright}</span>
+                <span className="card-date"> Date: {post_info[post].date}</span>
+              </div>
+              <div className="card-images">
+                <img src={post_info[post].hdurl} alt="" />
+              </div> 
+              <p className="card-explanation">{post_info[post].explanation}</p>
+              <div className="card-btn-block">
+                <button className="card-btn like">Like</button>
+                <button className="card-btn comment" onClick={()=> this.isCommentOpen_Two()}>Comments</button>
+                <button className="card-btn share">Share</button>
+              </div>
+              <ul className={`card-comment ${this.state.isCommentOpen_Two ? "open" : "close"}`}>
+                {
+                  comment.map((user) => (
+                    <li className="user">
+                      <span className="user-id">{user.id}</span>
+                      <span className="user-comment">{user.comment}</span>
+                    </li>
+                  ))
+                } 
+                <button className="load-btn">Load More...</button>
+              </ul>
+            </div>
+          ))
+        }
+        {
+          post_info.map((index, post) => (
+            <div key={index} className="card">
+              <h4 className="card-title">{post_info[post].title}</h4>
+              <div className="card-detail">
+                <span className="card-author">Author: {post_info[post].copyright}</span>
+                <span className="card-date"> Date: {post_info[post].date}</span>
+              </div>
+              <div className="card-images">
+                <img src={post_info[post].hdurl} alt="" />
+              </div> 
+              <p className="card-explanation">{post_info[post].explanation}</p>
+              <div className="card-btn-block">
+                <button className="card-btn like">Like</button>
+                <button className="card-btn comment" onClick={()=> this.isCommentOpen_Three()}>Comments</button>
+                <button className="card-btn share">Share</button>
+              </div>
+              <ul className={`card-comment ${this.state.isCommentOpen_Three ? "open" : "close"}`}>
+                {
+                  comment.map((user) => (
+                    <li className="user">
+                      <span className="user-id">{user.id}</span>
+                      <span className="user-comment">{user.comment}</span>
+                    </li>
+                  ))
+                } 
+                <button className="load-btn">Load More...</button>
+              </ul>
             </div>
           ))
         }
